edc12e0782071000ea9bf13cc7dc8bb8d70eb7db
Abhishek T C <abhishekgowda4258@gmail.com>
2021-02-09 20:53:40 +0530

    first commit

---
+++ src/app/auth/user.model.ts
@@ -0,0 +1,16 @@
+export class User {
+  constructor(
+    public userId: string,
+    public username: string,
+    private tokenData: string,
+    private expirationDate: Date
+  ) {}
+
+  get token() {
+    if (!this.expirationDate || new Date() > this.expirationDate) {
+      return null;
+    }
+    return this.tokenData;
+  }
+}
+  
